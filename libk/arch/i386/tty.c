b3c9ef731dde0310d95b5be7903d53e395a99b6b
Gabi Moldovan <gabi_250@live.com>
2021-12-27 00:06:54 +0000

    Scroll when the screen is filled.

--- libk/arch/i386/tty.c
+++ libk/arch/i386/tty.c
@@ -15,6 +15,33 @@ static size_t tty_column;
 static uint8_t tty_color;
 static uint16_t *vga_memory = VGA_MEMORY;
 
+static void
+tty_adjust_row() {
+    if (++tty_row == VGA_HEIGHT) {
+        for (size_t y = 0; y < VGA_HEIGHT - 1; y++) {
+            for (size_t x = 0; x < VGA_WIDTH; x++) {
+                const size_t index_old = y * VGA_WIDTH + x;
+                const size_t index_new = (y + 1) * VGA_WIDTH + x;
+                vga_memory[index_old] = vga_memory[index_new];
+            }
+        }
+        // Clear the last line
+        for (size_t x = 0; x < VGA_WIDTH; x++) {
+            const size_t index = (VGA_HEIGHT - 1) * VGA_WIDTH + x;
+            vga_memory[index] = vga_entry(' ', tty_color);
+        }
+        tty_row = VGA_HEIGHT - 1;
+    }
+}
+
+static void
+tty_adjust_indices() {
+    if (++tty_column == VGA_WIDTH) {
+        tty_column = 0;
+        tty_adjust_row();
+    }
+}
+
 void
 init_tty() {
     tty_row = 0;
@@ -33,19 +60,14 @@ void
 tty_putchar(char c) {
     const size_t index = tty_row * VGA_WIDTH + tty_column;
     vga_memory[index] = vga_entry((unsigned char)c, tty_color);
-    if (++tty_column == VGA_WIDTH) {
-        tty_column = 0;
-        if (++tty_row == VGA_HEIGHT) {
-            tty_row = 0;
-        }
-    }
+    tty_adjust_indices();
 }
 
 void
 tty_puts(const char *str, size_t size) {
     for (size_t i = 0; i < size; i++) {
         if (str[i] == '\n') {
-            ++tty_row;
+            tty_adjust_row();
             tty_column = 0;
         } else {
             tty_putchar(str[i]);
