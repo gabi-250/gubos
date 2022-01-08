3f89ad2ae41b4061787d45cf7ce02b39e5155edd
Gabi Moldovan <gabi_250@live.com>
2022-01-08 17:02:09 +0000

    Structure libk more like libc for consistency.

---
+++ libk/string/memset.c
@@ -0,0 +1,10 @@
+#include <string.h>
+
+void *
+memset(void *ptr, int value, size_t num) {
+    unsigned char *buf = (unsigned char *) ptr;
+    for (size_t i = 0; i < num; i++) {
+        buf[i] = (unsigned char) value;
+    }
+    return ptr;
+}
