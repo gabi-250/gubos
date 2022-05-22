466e869ea095f01478b71d3520e50cc2b472e074
Gabi Moldovan <gabi_250@live.com>
2022-05-22 17:57:53 +0100

    Document GDT flags, add an initial TSS.

--- kernel/arch/i386/gdt.c
+++ kernel/arch/i386/gdt.c
@@ -1,55 +1,29 @@
 #include <stdint.h>
 #include "gdt.h"
+#include "string.h"
 
 #define GDT_ENTRY_COUNT 5
 
 extern void load_gdt(uint32_t, uint16_t, uint16_t);
-
-typedef struct segment_descriptor {
-    // Bits 0-15 of the segment limit.
-    uint16_t limit_low;
-    // Bits 0-15 of the segment base.
-    uint16_t base_low;
-    // Bits 16-23 of the segment base.
-    uint8_t base_middle;
-    // Specifies the kind of access that can be made to the segment.
-    //
-    // See Section 3.4.5.1 ("Code- and Data-Segment Descriptor Types") of the
-    // Intel manual for more details.
-    //
-    // For example, 0b0010 should be used for a Read/Write data descriptor,
-    // while 0b1010 can be used for an Execute/Read code descriptor.
-    uint8_t access_type;
-    // | 3      0 | 19     16 |
-    // | flags    | limit     |
-    //
-    // where "limit" contains bits 16-19 of the segment limit, and the flags are:
-    // | 3 |  2  |  1 |  0  |
-    // | G | D/B | L  | AVL |
-    //
-    // G   - the granularity flag
-    // D/B - the default operation size flag (0 = 16-bit segment, 1 = 32-bit segment)
-    // L   - the 64-bit code segment flag. If set, the segment is a 64-bit code segment
-    // AVL - "available for use by system software", so in other words, reserved
-    //       and should be set to 0
-    uint8_t flags;
-    // Bits 24-31 of the segment base.
-    uint8_t base_high;
-} __attribute__((packed)) segment_descriptor_t;
-
-typedef struct gdt_descriptor {
-    uint16_t limit;
-    uint32_t base;
-} __attribute__((packed)) gdt_descriptor_t;
 
 static segment_descriptor_t gdt_entries[GDT_ENTRY_COUNT];
 
 // A pointer to the 48-bit GDT structure (this needs to be passed to lgdt)
 static gdt_descriptor_t gdt;
 
+// The task-state segment.
+static task_state_segment_t tss;
+
 static void
-set_segment_descriptor(uint32_t i, uint32_t base, uint32_t limit,
-        uint8_t access_type)
+tss_init() {
+    memset(&tss, sizeof(tss), 0);
+    tss.ss0 = GDT_KERNEL_DATA_SEGMENT;
+    tss.esp0 = 0; // TODO pick a stack address
+    // XXX what about io_map_base_address?
+}
+
+static void
+set_segment_descriptor(uint32_t i, uint32_t base, uint32_t limit, uint8_t access_type, uint8_t flags)
 {
     // The lower 16 bits of base:
     gdt_entries[i].base_low = base & 0xffff;
@@ -61,27 +35,39 @@ set_segment_descriptor(uint32_t i, uint32_t base, uint32_t limit,
     // The lower 16 bits of limit:
     gdt_entries[i].limit_low = limit & 0xffff;
     gdt_entries[i].flags = (limit >> 16) & 0x0f;
-    // Set these flags (0b1100):
-    // G   = 1
-    // D/B = 1
-    // L   = 0
-    // AVL = 0
-    gdt_entries[i].flags |= 0b1100 << 4;
+    gdt_entries[i].flags |= flags << 4;
 }
 
 void
 gdt_init() {
+    // NOTE: TSS is used to build the kernel stack to use for interrupt handling
+    // (whenever an interrupt occurs in userspace, the CPU needs to be able to
+    // prepare the kernel stack before switching to ring 0 for interrupt handling).
+    tss_init();
     // The NULL segment
-    set_segment_descriptor(0, 0, 0, 0);
+    set_segment_descriptor(0, 0, 0, 0, 0);
+
     // The kernel code descriptor
-    set_segment_descriptor(1, 0, 0xFFFFFFFF, 0x9A);
+    uint8_t kernel_code_desc_flags =
+        GDT_SEGMENT_PRESENT_FLAG | GDT_CODE_OR_DATA_DESCRIPTOR_FLAG | GDT_CODE_EXECUTE_READ_SEG_TYPE;
+    set_segment_descriptor(1, 0, 0xFFFFFFFF, kernel_code_desc_flags, GDT_GRANULARITY_FLAG | GDT_DB_FLAG);
+
     // The kernel data descriptor
-    set_segment_descriptor(2, 0, 0xFFFFFFFF, 0x92);
+    uint8_t kernel_data_desc_flags =
+        GDT_SEGMENT_PRESENT_FLAG | GDT_CODE_OR_DATA_DESCRIPTOR_FLAG | GDT_DATA_READ_WRITE_SEG_TYPE;
+    set_segment_descriptor(2, 0, 0xFFFFFFFF, kernel_data_desc_flags, GDT_GRANULARITY_FLAG | GDT_DB_FLAG);
+
     // The user-mode code descriptor
-    set_segment_descriptor(3, 0, 0xFFFFFFFF, 0xFA);
+    set_segment_descriptor(3, 0, 0xFFFFFFFF, kernel_code_desc_flags | GDT_DPL3_FLAG, GDT_GRANULARITY_FLAG | GDT_DB_FLAG);
+
     // The user-mode data descriptor
-    set_segment_descriptor(4, 0, 0xFFFFFFFF, 0xF2);
+    set_segment_descriptor(4, 0, 0xFFFFFFFF, kernel_data_desc_flags | GDT_DPL3_FLAG, GDT_GRANULARITY_FLAG | GDT_DB_FLAG);
+
+    // The TSS descriptor
+    uint8_t tss_desc_flags = GDT_SEGMENT_PRESENT_FLAG | GDT_CODE_EXECUTE_ONLY_ACCESSED;
+    set_segment_descriptor(5, (uint32_t)&tss, sizeof(task_state_segment_t) - 1, tss_desc_flags, 0x0);
     gdt.base = (uint32_t)gdt_entries;
     gdt.limit = (sizeof(segment_descriptor_t) * GDT_ENTRY_COUNT) - 1;
     load_gdt((uint32_t)&gdt, GDT_KERNEL_CODE_SEGMENT, GDT_KERNEL_DATA_SEGMENT);
+    // TODO call LTR
 }
--- kernel/arch/i386/include/gdt.h
+++ kernel/arch/i386/include/gdt.h
@@ -5,9 +5,123 @@
 #include <stdbool.h>
 
 // These must be kept in sync with the gdt_init logic
-#define GDT_KERNEL_CODE_SEGMENT 0x8
-#define GDT_KERNEL_DATA_SEGMENT 0x10
-#define GDT_USER_CODE_SEGMENT 0x18
-#define GDT_USER_DATA_SEGMENT 0x20
+#define GDT_KERNEL_CODE_SEGMENT         0x8
+#define GDT_KERNEL_DATA_SEGMENT         0x10
+#define GDT_USER_CODE_SEGMENT           0x18
+#define GDT_USER_DATA_SEGMENT           0x20
+#define GDT_TASK_STATE_SEGMENT          0x28
+
+// The granularity flag
+#define GDT_GRANULARITY_FLAG             0b1000
+// The default operation size flag (0 = 16-bit segment, 1 = 32-bit segment).
+#define GDT_DB_FLAG                      0b100
+// The 64-bit code segment flag. If set, the segment is a 64-bit code segment.
+#define GDT_L_FLAG                       0b10
+// The segment present flag
+#define GDT_SEGMENT_PRESENT_FLAG         0b10000000
+// The code or data descriptor type (0 = system; 1 = code or data)
+#define GDT_CODE_OR_DATA_DESCRIPTOR_FLAG 0b00010000
+// The descriptor privilege level set to 3
+#define GDT_DPL3_FLAG                    0b01100000
+
+// ======================================================================
+// Code- and Data-Segment Descriptor Types
+// See Section 3.4.5.1 ("Code- and Data-Segment Descriptor Types") of the
+// Intel® 64 and IA-32 Architectures Software Developer's Manual, Volume 3.
+// ======================================================================
+
+// An execute/read code segment type.
+#define GDT_CODE_EXECUTE_READ_SEG_TYPE 0b1010
+// An execute-only (accessed) code segment type.
+#define GDT_CODE_EXECUTE_ONLY_ACCESSED 0b1001
+// A read/write data segment type.
+#define GDT_DATA_READ_WRITE_SEG_TYPE 0b0010
+
+typedef struct segment_descriptor {
+    // Bits 0-15 of the segment limit.
+    uint16_t limit_low;
+    // Bits 0-15 of the segment base.
+    uint16_t base_low;
+    // Bits 16-23 of the segment base.
+    uint8_t base_middle;
+    // Specifies the kind of access that can be made to the segment.
+    //
+    // See Section 3.4.5.1 ("Code- and Data-Segment Descriptor Types") of the
+    // Intel manual for more details.
+    //
+    // For example, 0b0010 should be used for a Read/Write data descriptor,
+    // while 0b1010 can be used for an Execute/Read code descriptor.
+    //
+    // | 7 | 6     5 | 4  | 3            0 |
+    // | P | DPL     | S  | segment type   |
+    uint8_t access_type;
+    // | 7      4 | 3     0 |
+    // | flags    | limit   |
+    //
+    // where "limit" contains bits 16-19 of the segment limit, and the flags are:
+    // | 3 |  2  |  1 |  0  |
+    // | G | D/B | L  | AVL |
+    //
+    // G   - the granularity flag
+    // D/B - the default operation size flag (0 = 16-bit segment, 1 = 32-bit segment)
+    // L   - the 64-bit code segment flag. If set, the segment is a 64-bit code segment
+    // AVL - "available for use by system software", so in other words, reserved
+    //       and should be set to 0
+    uint8_t flags;
+    // Bits 24-31 of the segment base.
+    uint8_t base_high;
+} __attribute__((packed)) segment_descriptor_t;
+
+typedef struct gdt_descriptor {
+    uint16_t limit;
+    uint32_t base;
+} __attribute__((packed)) gdt_descriptor_t;
+
+// See also: Figure 7-2 from Chapter 7 (Task Management) of
+// Intel® 64 and IA-32 Architectures Software Developer's Manual, Volume 3.
+typedef struct task_state_segment {
+    uint16_t previous_task_link;
+    uint16_t previous_task_link_reserved;
+    // The stack pointer to use when switching from user mode to kernel mode.
+    uint32_t esp0;
+    // The stack segment to use when switching from user mode to kernel mode.
+    uint16_t ss0;
+    uint16_t ss0_reserved;
+    uint32_t esp1;
+    uint16_t ss1;
+    uint16_t ss1_reserved;
+    uint32_t esp2;
+    uint16_t ss2;
+    uint16_t ss2_reserved;
+    uint32_t cr3;
+    uint32_t eip;
+    uint32_t eflags;
+    uint32_t eax;
+    uint32_t ecx;
+    uint32_t edx;
+    uint32_t ebx;
+    uint32_t esp;
+    uint32_t ebp;
+    uint32_t esi;
+    uint32_t edi;
+    uint16_t es;
+    uint16_t es_reserved;
+    uint16_t cs;
+    uint16_t cs_reserved;
+    uint16_t ss;
+    uint16_t ss_reserved;
+    uint16_t ds;
+    uint16_t ds_reserved;
+    uint16_t fs;
+    uint16_t fs_reserved;
+    uint16_t gs;
+    uint16_t gs_reserved;
+    uint16_t ldt_segment_selector;
+    uint16_t ldt_segment_selector_reserved;
+    // Bits 1-15 are reserved. Bit 0 is the T (debug trap) flag.
+    uint16_t debug_trap;
+    uint16_t io_map_base_address;
+    uint32_t ssp;
+} __attribute__((packed)) task_state_segment_t;
 
 #endif /* __GDT_H__ */
